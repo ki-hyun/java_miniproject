463de333790041a4ca223249d9cb8d432c4c2dc6
k <inblue17@gmail.com>
2014-11-08 22:50:58 +0900

    q

--- javaHomework/src/javaHomework/imgFileCopy/Test02.java
+++ javaHomework/src/javaHomework/imgFileCopy/Test02.java
@@ -16,43 +16,45 @@ import java.io.IOException;
 
 public class Test02 {
 
-  public static void main(String[] args) {
-    FileInputStream in = null;
-    FileOutputStream out = null;
-    try {
+	public static void main(String[] args) {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		try {
 
+			// 현재 파일의 위치는 프로젝트 디렉토리이다.
+			in = new FileInputStream(args[0]);
+			String source = args[0];
+			// String newFileName =
+			String target = (source.substring(0, source.lastIndexOf('.')));
+			out = new FileOutputStream(target + "-01.jpg");
+			int b = -1;
 
-      // 현재 파일의 위치는 프로젝트 디렉토리이다.
-      in = new FileInputStream(args[0]);
-      String source = args[0];
-      //String newFileName = 
-        String target = (source.substring(0, source.lastIndexOf('.')));
-      out = new FileOutputStream(target + "-01.jpg");
-      int b = -1;
-      
-      //2. read()를 통해 1바이트 읽기
-      //리턴 타입이 int라 해서 4바이트를 읽는 것은 아니다.
-      while ((b = in.read()) != -1) {
-      
-        out.write(b);
-      }
-      //3. File이나 Detabase, Socket 등의 자원은 사용한 다음 
-      // 명시적으로 해제 해야 한다.
-      // 
+			// 2. read()를 통해 1바이트 읽기
+			// 리턴 타입이 int라 해서 4바이트를 읽는 것은 아니다.
+			while ((b = in.read()) != -1) {
 
-     
+				out.write(b);
+			}
+			// 3. File이나 Detabase, Socket 등의 자원은 사용한 다음
+			// 명시적으로 해제 해야 한다.
+			//
 
-    }catch (FileNotFoundException ex) {
-      System.out.println("img.jpg 파일을 찾을 수 없습니다.");
+		} catch (FileNotFoundException ex) {
+			System.out.println("img.jpg 파일을 찾을 수 없습니다.");
 
-    } catch (IOException ex) {
-      System.out.println("읽기 오류!");
+		} catch (IOException ex) {
+			System.out.println("읽기 오류!");
 
+		} finally {
+			try {
+				in.close();
+			} catch (IOException ex) {
+			}
+			try {
+				out.close();
+			} catch (IOException ex) {
+			}
+		}
 
-    }finally {
-      try{ in.close(); } catch(IOException ex) {}
-      try{ out.close(); } catch(IOException ex) {}
-    }
-
-  }
+	}
 }
--- javaHomework/src/test04/java63/annotation/Command.java
+++
@@ -1,9 +0,0 @@
-package test04.java63.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention (RetentionPolicy.RUNTIME)
-public @interface Command {
-  String value() default "";
-}
